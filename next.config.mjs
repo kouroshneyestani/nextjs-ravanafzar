fbaafbf47982c1aaf347d7c60a30f4daa5ab557f
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-02 16:18:48 +0330

    feat: add linter configs, tailwind RTL, and engines to package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import config from "./next-i18next.config.js";
+
+const { i18n } = config;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    i18n,
+    reactStrictMode: true,
+};
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const tailwindRtl = require("tailwindcss-rtl");
+
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter", "ui-sans-serif", "system-ui"],
+                persian: ["IRANSansWeb", "ui-sans-serif", "system-ui"], // Add Persian font
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [tailwindRtl],
 };
