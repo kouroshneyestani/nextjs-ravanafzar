f126f8d33be3eae5a4b8155ee0db525104e842cd
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-03 17:43:47 +0330

    feat: add grid & layout components

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export * from "./layout";
 export * from "./common";
 export * from "./forms";
 export * from "./icons";
+export * from "./grid";
 export * from "./ui";
---
+++ src/components/layout/index.js
@@ -0,0 +1,3 @@
+import SpaceBar from "./SpaceBar";
+
+export { SpaceBar };
