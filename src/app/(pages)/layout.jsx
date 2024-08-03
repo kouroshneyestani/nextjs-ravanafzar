9d8f6510b946e02bb1ae7f7d66c2a8ee868736b7
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-03 17:44:31 +0330

    feat: update main layout

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,47 +1,5 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-
 export default function Layout({ children }) {
-    const pathname = usePathname();
-    const links = [
-        {
-            href: "/",
-            name: "خانه",
-        },
-        {
-            href: "/quizzes",
-            name: "تست های روان شناسی",
-        },
-        {
-            href: "/blog",
-            name: "وبلاگ",
-        },
-    ];
-
-    return (
-        <section>
-            {/* <header>
-                <nav>
-                    <ul>
-                        {links.map((link, index) => (
-                            <li key={index}>
-                                <Link
-                                    href={link.href}
-                                    className={`${
-                                        pathname === link.href && "text-red-600"
-                                    }`}
-                                >
-                                    {link.name}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-            </header> */}
-
-            {children}
-        </section>
-    );
+    return <section>{children}</section>;
 }
