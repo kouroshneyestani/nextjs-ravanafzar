335105a06b5e3386ac61c7a917eb551d5f521233
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-03 16:02:42 +0330

    perf: improve performance

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({
-    color,
+    color = "primary",
     children,
     className,
     onClick,
--- src/components/ui/Stepper.js
+++ src/components/ui/Stepper.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import { StepContext } from "../../contexts/StepperContext";
 import { useFetch, useSubmitData } from "../../hooks";
 import {
@@ -19,10 +19,13 @@ const Stepper = ({ className }) => {
         error: submitError,
         submitData,
     } = useSubmitData("/submit-endpoint");
+
     const [answers, setAnswers] = useState({});
-    const [submitStatus, setSubmitStatus] = useState(null);
-    const [isTransitioning, setIsTransitioning] = useState(false);
-    const [isSurveyComplete, setIsSurveyComplete] = useState(false);
+    const [state, setState] = useState({
+        submitStatus: null,
+        isTransitioning: false,
+        isSurveyComplete: false,
+    });
 
     useEffect(() => {
         if (questions && questions.length > 0 && currentStep === 0) {
@@ -30,22 +33,36 @@ const Stepper = ({ className }) => {
         }
     }, [questions, currentStep]);
 
-    const handleTransition = (direction) => {
-        if (isSurveyComplete) return;
+    const handleTransition = useCallback(
+        (direction) => {
+            if (state.isSurveyComplete) return;
 
-        setIsTransitioning(true);
-        setTimeout(() => {
-            direction === "next" ? nextStep() : previousStep();
-            setIsTransitioning(false);
-        }, 300);
-    };
+            setState((prevState) => ({ ...prevState, isTransitioning: true }));
+            setTimeout(() => {
+                direction === "next" ? nextStep() : previousStep();
+                setState((prevState) => ({
+                    ...prevState,
+                    isTransitioning: false,
+                }));
+            }, 300);
+        },
+        [nextStep, previousStep, state.isSurveyComplete],
+    );
 
     const handleOptionChange = (value) => {
-        setAnswers({ ...answers, [currentStep]: value });
-
+        setAnswers((prev) => ({ ...prev, [currentStep]: value }));
         if (currentStep < questions.length - 1) {
             handleTransition("next");
         }
+    };
+
+    const handleFinish = async () => {
+        const result = await submitData(answers);
+        setState((prevState) => ({
+            ...prevState,
+            submitStatus: result ? "success" : "error",
+            isSurveyComplete: !!result,
+        }));
     };
 
     if (loading) return <Loading />;
@@ -57,117 +74,105 @@ const Stepper = ({ className }) => {
     const isFinishDisabled =
         isLastStep && currentQuestion.required && !answers[currentStep];
 
-    const handleFinish = async () => {
-        const result = await submitData(answers);
-        if (result) {
-            setSubmitStatus("success");
-            setIsSurveyComplete(true);
-        } else {
-            setSubmitStatus("error");
-        }
-    };
-
     return (
-        <div className={`relative`}>
+        <div className="relative">
             <ProgressBar
                 className="absolute top-0 left-0"
                 totalSteps={questions.length}
                 currentStep={
-                    isSurveyComplete ? questions.length - 1 : currentStep
+                    state.isSurveyComplete ? questions.length - 1 : currentStep
                 }
             />
             {currentStep < questions.length ? (
-                <>
-                    <div
-                        className={`${className} relative transition-transform duration-300 transform ${
-                            isTransitioning
-                                ? "scale-95 opacity-50"
-                                : "scale-100 opacity-100"
-                        }`}
-                    >
-                        <header className="flex flex-col gap-4">
-                            <div className="flex gap-2 text-lg font-bold text-primary">
-                                <span className="text-xl">پرسش</span>
-                                <div className="flex gap-1">
-                                    <span>{currentStep + 1}</span>
-                                    <span>از</span>
-                                    <span>{questions.length}</span>
-                                </div>
+                <div
+                    className={`${className} relative transition-transform duration-300 transform ${state.isTransitioning ? "scale-95 opacity-50" : "scale-100 opacity-100"}`}
+                >
+                    <header className="flex flex-col gap-4">
+                        <div className="flex gap-2 text-lg font-bold text-primary">
+                            <span className="text-xl">پرسش</span>
+                            <div className="flex gap-1">
+                                <span>{currentStep + 1}</span>
+                                <span>از</span>
+                                <span>{questions.length}</span>
                             </div>
-                            <h4 className="text-3xl font-bold mb-2">
-                                {currentQuestion.body}
-                            </h4>
-                        </header>
+                        </div>
+                        <h4 className="text-3xl font-bold mb-2">
+                            {currentQuestion.body}
+                        </h4>
+                    </header>
 
-                        <article className="flex flex-col gap-2">
-                            {currentQuestion.options.map((option, index) => (
-                                <RadioBox
-                                    key={index}
-                                    option={option}
-                                    isSelected={answers[currentStep] === option}
-                                    onChange={handleOptionChange}
-                                    name={`question-${currentStep}`}
-                                />
-                            ))}
-                        </article>
+                    <article className="flex flex-col gap-2">
+                        {currentQuestion.options.map((option, index) => (
+                            <RadioBox
+                                key={index}
+                                option={option}
+                                isSelected={answers[currentStep] === option}
+                                onChange={handleOptionChange}
+                                name={`question-${currentStep}`}
+                            />
+                        ))}
+                    </article>
 
-                        <footer className="mx-auto flex justify-between items-center gap-4">
+                    <footer className="mx-auto flex justify-between items-center gap-4">
+                        <Button
+                            onClick={() => handleTransition("prev")}
+                            disabled={currentStep === 0}
+                        >
+                            سوال قبلی
+                            <LongArrowRight width={21} />
+                        </Button>
+
+                        {!isLastStep ? (
                             <Button
-                                color="primary"
-                                onClick={() => handleTransition("prev")}
-                                disabled={currentStep === 0}
+                                onClick={() => handleTransition("next")}
+                                disabled={isNextDisabled}
                             >
-                                سوال قبلی
-                                <LongArrowRight width={21} />
+                                سوال بعدی
+                                <LongArrowLeft width={21} />
                             </Button>
-
-                            {isLastStep ? (
-                                <Button
-                                    color="success"
-                                    onClick={handleFinish}
-                                    disabled={isSubmitting || isFinishDisabled}
-                                >
-                                    {isSubmitting
-                                        ? "در حال پردازش..."
-                                        : "دیدن نتایج"}
-                                    <LongArrowLeft width={21} />
-                                </Button>
-                            ) : (
-                                <Button
-                                    color="primary"
-                                    onClick={() => handleTransition("next")}
-                                    disabled={isNextDisabled}
-                                >
-                                    سوال بعدی
-                                    <LongArrowLeft width={21} />
-                                </Button>
-                            )}
-                        </footer>
-                    </div>
-                </>
-            ) : (
-                <div className="flex flex-col items-center">
-                    <div className="text-lg font-bold mb-4">نتایج تست</div>
-                    {submitStatus === "success" && (
-                        <div className="mt-4 text-success">
-                            <div>با موفقیت ثبت شد.</div>
-                            <div>{response}</div>
-                        </div>
-                    )}
-                    {submitStatus === "error" && (
-                        <div className="mt-4 text-danger">
-                            <div>مشکلی پیش آمده. لطفا دوباره امتحان کنید.</div>
-                        </div>
-                    )}
-                    {submitError && (
-                        <div className="mt-4 text-danger">
-                            ارور: {submitError}
-                        </div>
-                    )}
+                        ) : (
+                            <Button
+                                color="success"
+                                onClick={handleFinish}
+                                disabled={isSubmitting || isFinishDisabled}
+                            >
+                                {isSubmitting
+                                    ? "در حال پردازش..."
+                                    : "دیدن نتایج"}
+                                <LongArrowLeft width={21} />
+                            </Button>
+                        )}
+                    </footer>
                 </div>
+            ) : (
+                <Results
+                    submitStatus={state.submitStatus}
+                    response={response}
+                    submitError={submitError}
+                />
             )}
         </div>
     );
 };
+
+const Results = ({ submitStatus, response, submitError }) => (
+    <div className="flex flex-col items-center">
+        <div className="text-lg font-bold mb-4">نتایج تست</div>
+        {submitStatus === "success" && (
+            <div className="mt-4 text-success">
+                <div>با موفقیت ثبت شد.</div>
+                <div>{response}</div>
+            </div>
+        )}
+        {submitStatus === "error" && (
+            <div className="mt-4 text-danger">
+                <div>مشکلی پیش آمده. لطفا دوباره امتحان کنید.</div>
+            </div>
+        )}
+        {submitError && (
+            <div className="mt-4 text-danger">ارور: {submitError}</div>
+        )}
+    </div>
+);
 
 export default Stepper;
