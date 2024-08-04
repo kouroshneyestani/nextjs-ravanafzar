a792eed9f018019e1d98b47ddb1bee265e1a818a
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-04 19:37:34 +0330

    fix: add Container in Quiz component

--- src/app/(pages)/quizzes/page.jsx
+++ src/app/(pages)/quizzes/page.jsx
@@ -1,18 +1,19 @@
 "use client";
 
-import Link from "next/link";
 import { quizzes } from "@data";
-import { CardQuiz } from "@components";
+import { Container, CardQuiz } from "@components";
 
 export default function Page() {
     return (
         <>
             <main className="w-full">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {quizzes.map((item) => (
-                        <CardQuiz key={item.id} {...item} />
-                    ))}
-                </div>
+                <Container>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {quizzes.map((item) => (
+                            <CardQuiz key={item.id} {...item} />
+                        ))}
+                    </div>
+                </Container>
             </main>
         </>
     );
