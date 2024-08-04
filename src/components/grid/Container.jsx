ccaf07cdbaf7b73e3f5fb21cab6d744734d58e8a
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-04 17:18:42 +0330

    perf: improve spacing for Container component

--- src/components/grid/Container.jsx
+++ src/components/grid/Container.jsx
@@ -1,7 +1,7 @@
 export default function Container({ children, className }) {
     return (
         <div
-            className={`container mx-auto max-w-screen-xl xl:px-0 ${className}`}
+            className={`container mx-auto max-w-[1100px] px-20 xl:px-0 ${className}`}
         >
             {children}
         </div>
