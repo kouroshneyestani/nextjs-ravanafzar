9a9d5f42c0dd1f2edcfc7ce3cf393ad775904b6e
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-04 17:19:16 +0330

    feat: add quizzes and users to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,43 @@
+import { consultants, quizzes } from "@data";
+import { Container, CardConsultants, CardQuiz } from "@components";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <p className="bg-sucess fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    Get started by editing&nbsp;
-                    <code className="font-mono font-bold">src/app/page.js</code>
-                </p>
+        <>
+            <div className="flex flex-col gap-48">
+                <section className="w-full flex flex-col gap-24">
+                    <header>
+                        <Container>
+                            <h2 className="text-4xl font-bold">
+                                برترین مشاورین
+                            </h2>
+                        </Container>
+                    </header>
+                    <Container>
+                        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                            {consultants.map((item) => (
+                                <CardConsultants key={item.id} {...item} />
+                            ))}
+                        </div>
+                    </Container>
+                </section>
+                <section className="w-full flex flex-col gap-24">
+                    <header>
+                        <Container>
+                            <h2 className="text-4xl font-bold">
+                                تست های روان شناسی
+                            </h2>
+                        </Container>
+                    </header>
+                    <Container>
+                        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                            {quizzes.map((item) => (
+                                <CardQuiz key={item.id} {...item} />
+                            ))}
+                        </div>
+                    </Container>
+                </section>
             </div>
-        </main>
+        </>
     );
 }
