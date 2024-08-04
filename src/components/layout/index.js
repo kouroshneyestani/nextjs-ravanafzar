18ed5cffc1cb139590420088e2db9479a28aba96
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-04 12:10:14 +0330

    fix: remove deleted component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,10 @@
+// Other
 import SpaceBar from "./SpaceBar";
+
 export { SpaceBar };
 
-import CustomTransition from "./CustomTransition";
-export { CustomTransition };
-
+// Header
 import Header1 from "./header/Header1";
 import HeaderBackButton from "./header/HeaderBackButton";
+
 export { Header1, HeaderBackButton };
