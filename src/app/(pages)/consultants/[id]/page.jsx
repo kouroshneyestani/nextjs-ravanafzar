4c279311d4f23aa5a63d564f91d62fe18c513e6a
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-04 19:37:02 +0330

    feat: add & use Quiz and User components in related pages

--- src/app/(pages)/consultants/[id]/page.jsx
+++ src/app/(pages)/consultants/[id]/page.jsx
@@ -1,16 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import Image from "next/image";
-import {
-    Button,
-    Header1,
-    SpaceBar,
-    Container,
-    TomanIcon,
-    ClockIcon,
-    BarChartIcon,
-} from "@components";
+import { Container, CardConsultants, Button } from "@components";
 
 const data = [
     {
@@ -32,126 +22,40 @@ const data = [
 
 export default function Page() {
     return (
-        <div className="w-full flex gap-10 b-red-300">
-            <aside className="w-2/5">
-                <div className="top-10 sticky">
-                    {data.map((item) => (
-                        <div key={item.id}>
-                            <div className="flex flex-col gap-4">
-                                <div className="flex items-center gap-4">
-                                    <div className="w-20 h-20 relative  overflow-hidden rounded-default">
-                                        <Link href={`/quizzes/${item.id}`}>
-                                            <Image
-                                                fill
-                                                alt={item.title}
-                                                src={item.image}
-                                                objectFit="cover"
-                                            />
-                                        </Link>
-                                    </div>
-                                    <div className="flex flex-col gap-2 mt-2">
-                                        <h3 className="text-xl font-bold hover:text-primary transition-colors duration-100">
-                                            <Link
-                                                href={`/consultants/${item.id}`}
-                                            >
-                                                {item.title}
-                                            </Link>
-                                        </h3>
-                                        <p>{item.subtitle}</p>
-                                    </div>
-                                </div>
-                                <div className="pt-3 pb-3">
-                                    <div className="flex items-center justify-around gap-10 text-sm">
-                                        <div className="flex flex-col items-center gap-1">
-                                            <div className="flex flex-col gap-2">
-                                                <div className="flex gap-2">
-                                                    <ClockIcon
-                                                        width={15}
-                                                        height={15}
-                                                    />
-                                                    <span className="flex gap-1">
-                                                        <span>
-                                                            {item.readingTime}
-                                                        </span>
-                                                        <span>دقیقه</span>
-                                                    </span>
-                                                </div>
-                                                <div className="flex gap-2">
-                                                    <BarChartIcon
-                                                        width={15}
-                                                        height={15}
-                                                    />
-                                                    <span className="flex gap-1">
-                                                        <span>
-                                                            {item.questions}
-                                                        </span>
-                                                        <span>پرسش</span>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <span className="w-[1px] h-10 flex bg-gray" />
-                                        <div className="flex flex-col gap-1">
-                                            {item.off ? (
-                                                <>
-                                                    <div className="flex gap-1 ">
-                                                        <span className="text-lg">
-                                                            {parseInt(
-                                                                item.price *
-                                                                    (1 -
-                                                                        item.off /
-                                                                            100),
-                                                            ).toLocaleString()}
-                                                        </span>
-                                                        <span>
-                                                            <TomanIcon />
-                                                        </span>
-                                                    </div>
-                                                    <div className="flex gap-1 ">
-                                                        <span className="line-through opacity-50">
-                                                            {item.price.toLocaleString()}
-                                                        </span>
-                                                    </div>
-                                                </>
-                                            ) : (
-                                                <div className="flex gap-1 ">
-                                                    <span className="text-lg">
-                                                        {item.price.toLocaleString()}
-                                                    </span>
-                                                    <span>
-                                                        <TomanIcon />
-                                                    </span>
-                                                </div>
-                                            )}
-                                        </div>
-                                    </div>
-                                </div>
-                                <span className="w-full h-[1px] flex bg-gray" />
-                                <Button color="primary" className="w-full">
-                                    <span>رزرور وقت مشاوره</span>
+        <>
+            <Container>
+                <div className="w-full flex gap-10">
+                    <aside className="w-2/5">
+                        <div className="top-10 sticky">
+                            <div className="flex flex-col gap-5">
+                                {data.map((item) => (
+                                    <CardConsultants key={item.id} {...item} />
+                                ))}
+                                <Button className="w-full">
+                                    <span>رزرو وقت مشاوره</span>
                                 </Button>
                             </div>
                         </div>
-                    ))}
+                    </aside>
+                    <main className="w-5/6">
+                        <div className="w-full h-screen bg-gray-300 border-r-[1px] border-overlay">
+                            <nav className="top-0 sticky">
+                                <ul className="flex items-end justify-around border-b border-overlay">
+                                    <li className="h-16 px-10 font-bold flex items-center justify-center border-b-4 border-primary text-primary">
+                                        دیدگاه کاربران
+                                    </li>
+                                    <li className="h-16 px-10 font-bold flex items-center justify-center border-b-4 border-transparent">
+                                        سوابقه و مشخصات{" "}
+                                    </li>
+                                    <li className="h-16 px-10 font-bold flex items-center justify-center border-b-4 border-transparent">
+                                        رزومه و افتخارات
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </main>
                 </div>
-            </aside>
-            <main className="w-5/6">
-                <div className="w-full h-screen bg-gray-300 shadow rounded-default">
-                    <nav className="top-0 sticky">
-                        <ul className="flex items-end justify-around border-b border-overlay">
-                            <li className="h-16 px-10 font-bold flex items-center justify-center border-b-4 border-primary text-primary">
-                                دیدگاه کاربران
-                            </li>
-                            <li className="h-16 px-10 font-bold flex items-center justify-center border-b-4 border-transparent">
-                                سوابقه و مشخصات{" "}
-                            </li>
-                            <li className="h-16 px-10 font-bold flex items-center justify-center border-b-4 border-transparent">
-                                رزومه و افتخارات
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </main>
-        </div>
+            </Container>
+        </>
     );
 }
--- src/app/(pages)/consultants/page.jsx
+++ src/app/(pages)/consultants/page.jsx
@@ -2,17 +2,19 @@
 
 import Link from "next/link";
 import { consultants } from "@data";
-import { CardConsultants } from "@components";
+import { Container, CardConsultants } from "@components";
 
 export default function Page() {
     return (
         <>
             <main className="w-full">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {consultants.map((item) => (
-                        <CardConsultants key={item.id} {...item} />
-                    ))}
-                </div>
+                <Container>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {consultants.map((item) => (
+                            <CardConsultants key={item.id} {...item} />
+                        ))}
+                    </div>
+                </Container>
             </main>
         </>
     );
