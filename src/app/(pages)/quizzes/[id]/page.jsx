af90abfc8b0a1e55b9e04da1e95e5b147b4e267f
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-04 10:30:02 +0330

    feat: add test landing component before starting test stepper

--- src/app/(pages)/quizzes/[id]/page.jsx
+++ src/app/(pages)/quizzes/[id]/page.jsx
@@ -1,13 +1,90 @@
 "use client";
 
-import { SpaceBar, Stepper } from "../../../../components";
+import { useState } from "react";
+import { SpaceBar, Stepper, Button, TomanIcon } from "../../../../components";
 
 export default function Page() {
+    const [landing, setLanding] = useState(true);
+
+    const startTest = () => setLanding(false);
+    const onAddToCart = () => {
+        alert("دروازه پرداخت");
+        setLanding(false);
+    };
+
     return (
         <>
-            <div>
-                <Stepper className="max-w-xl h-screen flex flex-col gap-10 justify-between mx-auto select-none pb-6 pt-16" />
+            <div className="w-full h-screen flex flex-col items-center justify-center">
+                {landing ? (
+                    <TestIntro
+                        title="تست شرمساری از بدن"
+                        subtitle="این مجموعه سوالات به احساس رضایت از ظاهر شما می‌پردازد. توجه داشته باشید که نتایج، به صورت مطلق نشان‌دهنده وجود یا عدم شرمساری از بدن نیست و بهتر است این موضوع با نظارت و همراهی یک متخصص، بررسی شود."
+                        ages={18}
+                        time={180}
+                        questions={80}
+                        price={240000}
+                        onStartTest={startTest}
+                        onAddToCart={onAddToCart}
+                    />
+                ) : (
+                    <Stepper className="max-w-xl w-lvw h-screen flex flex-col gap-10 justify-between mx-auto select-none pb-6 pt-16" />
+                )}
             </div>
         </>
+    );
+}
+
+function TestIntro({
+    title,
+    subtitle,
+    questions,
+    time,
+    ages,
+    price,
+    onStartTest,
+    onAddToCart,
+}) {
+    return (
+        <div className="max-w-2xl flex flex-col gap-10 justify-between mx-auto select-none pb-6 pt-16">
+            <div className="text-center flex flex-col gap-6">
+                {title && <h1 className="font-bold text-4xl">{title}</h1>}
+                {subtitle && <p className="text-lg">{subtitle}</p>}
+                <ul className="gap-10 flex items-center justify-center my-10">
+                    {questions && (
+                        <li className="flex flex-col gap-4">
+                            <span class="font-bold text-lg">تعداد سوالات</span>
+                            <span>{questions}</span>
+                        </li>
+                    )}
+                    {time && (
+                        <li className="flex flex-col gap-4">
+                            <span class="font-bold text-lg">
+                                زمان مورد نیاز
+                            </span>
+                            <span>{time}</span>
+                        </li>
+                    )}
+                    {ages && (
+                        <li className="flex flex-col gap-4">
+                            <span class="font-bold text-lg">رده سنسی</span>
+                            <span>{ages}</span>
+                        </li>
+                    )}
+                </ul>
+                <div className="flex items-center justify-center">
+                    {price ? (
+                        <Button onClick={onAddToCart}>
+                            <span>شروع آزمون:</span>
+                            <div className="flex items-center justify-end">
+                                {price.toLocaleString()}
+                                <TomanIcon />
+                            </div>
+                        </Button>
+                    ) : (
+                        <Button onClick={onStartTest}>شروع آزمون</Button>
+                    )}
+                </div>
+            </div>
+        </div>
     );
 }
