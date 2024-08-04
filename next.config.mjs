c86bc11260257c3dc6b35c054ff3ac7eed0c1ae0
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-04 11:29:08 +0330

    chore: update nextjs config file, use alias

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 import config from "./next-i18next.config.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const { i18n } = config;
+
+// Convert the import.meta.url to __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,6 +15,14 @@ const nextConfig = {
         domains: ["images.unsplash.com"],
     },
     reactStrictMode: true,
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "@app": path.resolve(__dirname, "src/app"),
+            "@components": path.resolve(__dirname, "src/components"),
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
