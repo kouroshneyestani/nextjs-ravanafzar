4adfd265dfe00efbb51909bb2c2727a729d6034e
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-05 09:44:01 +0330

    perf: improve Button component

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,16 +2,24 @@ import React from "react";
 
 const Button = ({
     color = "primary",
+    size = "md",
     children,
     className,
     onClick,
     disabled = false,
     type = "button",
 }) => {
+    let buttonSize = "px-6 h-14";
+    let buttonColor = "bg-primary border-primary";
+
+    if (size === "sm") {
+        buttonSize = "px-4 h-10";
+    }
+
     return (
         <button
             type={type}
-            className={`h-14 px-6 font-bold rounded-default flex items-center justify-center gap-3 text-white border-2 border-${color} bg-${color} transition-opacity duration-300 ${className} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
+            className={`font-bold rounded-default flex items-center justify-center gap-3 text-white border-2 border-${color} bg-${color} transition-opacity duration-300 ${className} ${disabled ? "opacity-50 cursor-not-allowed" : ""} ${buttonSize}`}
             onClick={onClick}
             disabled={disabled}
         >
