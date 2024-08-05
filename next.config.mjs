974e2828ec8a1011488e1fec8bb5c9c3a5b728f4
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-05 10:53:40 +0330

    perf: add context alias for import contexts

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ const nextConfig = {
             ...config.resolve.alias,
             "@app": path.resolve(__dirname, "src/app"),
             "@data": path.resolve(__dirname, "src/data"),
+            "@contexts": path.resolve(__dirname, "src/contexts"),
             "@components": path.resolve(__dirname, "src/components"),
         };
         return config;
