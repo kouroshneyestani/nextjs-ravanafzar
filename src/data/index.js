bccca1cf36d7eabd6ae141ad2742f0e63391bdb1
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-05 09:43:00 +0330

    chore: add links & socials data

--- src/data/index.js
+++ src/data/index.js
@@ -1,2 +1,4 @@
+export * from "./links";
+export * from "./socials";
 export * from "./quizzes";
 export * from "./consultants";
---
+++ src/data/links.js
@@ -0,0 +1,57 @@
+export const links = [
+    {
+        href: "/",
+        text: "برگ نخست",
+        options: [],
+    },
+    {
+        href: "/consultants",
+        text: "مشاوره",
+        options: [],
+    },
+    {
+        href: "/quizzes",
+        text: "تست ها",
+        options: [
+            {
+                text: "تست شماره 1",
+                href: "/quizzes/1",
+            },
+            {
+                text: "تست شماره 2",
+                href: "/quizzes/2",
+            },
+            {
+                text: "تست شماره 3",
+                href: "/quizzes/3",
+            },
+        ],
+    },
+    {
+        href: "/partners",
+        text: "همکاری",
+        options: [],
+    },
+    {
+        href: "/contacts",
+        text: "تماس با ما",
+        options: [],
+    },
+];
+export const footerLinks = [
+    {
+        href: "/partners",
+        text: "همکاری",
+        options: [],
+    },
+    {
+        href: "/about",
+        text: "درباره ما",
+        options: [],
+    },
+    {
+        href: "/contacts",
+        text: "تماس با ما",
+        options: [],
+    },
+];
