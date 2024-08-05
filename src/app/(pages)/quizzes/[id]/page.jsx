ea24c39dc79f1f30806289dfc692fa089b582d7b
Kourosh <kouroshsharifineyestani@gmail.com>
2024-08-05 11:59:13 +0330

    perf: fix and improve performace

--- src/app/(pages)/quizzes/[id]/page.jsx
+++ src/app/(pages)/quizzes/[id]/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
-import { StepProvider } from "@contexts/StepperContext";
+import { useRouter, useParams } from "next/navigation";
+
 import {
-    Stepper,
     Button,
+    Loading,
     TomanIcon,
     ClockIcon,
     ProfileIcon,
@@ -13,101 +13,63 @@ import {
 } from "@components";
 
 export default function Page() {
-    const [view, setView] = useState("landing");
+    const router = useRouter();
+    const { id } = useParams();
 
-    const startTest = () => setView("landing");
     const onAddToCart = () => {
         alert("دروازه پرداخت");
-        setView("tests");
+
+        if (id) {
+            console.log(`Navigating to /quizzes/${id}/start`);
+            router.push(`/quizzes/${id}/start`);
+        }
     };
 
-    return (
-        <>
-            {view === "landing" ? (
-                <TestIntro
-                    title="تست روان شناسی شخصیت (NEO)"
-                    subtitle="این مجموعه سوالات به احساس رضایت از ظاهر شما می‌پردازد. توجه داشته باشید که نتایج، به صورت مطلق نشان‌دهنده وجود یا عدم شرمساری از بدن نیست و بهتر است این موضوع با نظارت و همراهی یک متخصص، بررسی شود."
-                    ages={18}
-                    time={180}
-                    questions={240}
-                    price={240000}
-                    onStartTest={startTest}
-                    onAddToCart={onAddToCart}
-                />
-            ) : (
-                <StepProvider>
-                    <Stepper className="w-lvw max-w-xl h-screen flex flex-col gap-10 justify-end mx-auto select-none " />
-                </StepProvider>
-            )}
-        </>
-    );
-}
+    if (!id) {
+        return <Loading />;
+    }
 
-function TestIntro({
-    title,
-    subtitle,
-    questions,
-    time,
-    ages,
-    price,
-    onStartTest,
-    onAddToCart,
-}) {
     return (
         <div className="flex flex-col gap-10 justify-between mx-auto select-none pb-6 pt-16">
             <div className="w-lvw max-w-4xl">
                 <div className="flex items-center gap-16">
                     <article>
                         <div className="flex flex-col gap-6">
-                            {title && (
-                                <h1 className="font-bold text-4xl">{title}</h1>
-                            )}
-                            {subtitle && <p className="text-lg">{subtitle}</p>}
+                            <h1 className="font-bold text-4xl">
+                                تست روان شناسی شخصیت (NEO)
+                            </h1>
+                            <p className="text-lg">
+                                این مجموعه سوالات به احساس رضایت از ظاهر شما
+                                می‌پردازد. توجه داشته باشید که نتایج، به صورت
+                                مطلق نشان‌دهنده وجود یا عدم شرمساری از بدن نیست
+                                و بهتر است این موضوع با نظارت و همراهی یک متخصص،
+                                بررسی شود.
+                            </p>
                             <ul className="gap-10 flex items-center mb-10">
-                                {questions && (
-                                    <li className="flex flex-col gap-4">
-                                        <span className="font-bold text-lg flex items-center gap-2">
-                                            <BarChartIcon />
-                                            <span className="mt-2">
-                                                تعداد سوالات
-                                            </span>
-                                        </span>
-                                        <span>{questions} پرسش</span>
-                                    </li>
-                                )}
-                                {time && (
-                                    <li className="flex flex-col gap-4">
-                                        <span className="font-bold text-lg flex items-center gap-2">
-                                            <ClockIcon />
-                                            <span>زمان مورد نیاز</span>
-                                        </span>
-                                        <span>{time} دقیقه</span>
-                                    </li>
-                                )}
-                                {ages && (
-                                    <li className="flex flex-col gap-4">
-                                        <span className="font-bold text-lg flex items-center gap-2">
-                                            <ProfileIcon />
-                                            <span>رده سنسی</span>
-                                        </span>
-                                        <span>{ages} سال</span>
-                                    </li>
-                                )}
+                                <TestDetail
+                                    icon={<BarChartIcon />}
+                                    label="تعداد سوالات"
+                                    value="240 پرسش"
+                                />
+                                <TestDetail
+                                    icon={<ClockIcon />}
+                                    label="زمان مورد نیاز"
+                                    value="180 دقیقه"
+                                />
+                                <TestDetail
+                                    icon={<ProfileIcon />}
+                                    label="رده سنی"
+                                    value="18 سال"
+                                />
                             </ul>
                             <div className="flex items-center">
-                                {price ? (
-                                    <Button onClick={onAddToCart}>
-                                        <span>شروع آزمون:</span>
-                                        <div className="flex items-center justify-end">
-                                            {price.toLocaleString()}
-                                            <TomanIcon />
-                                        </div>
-                                    </Button>
-                                ) : (
-                                    <Button onClick={onStartTest}>
-                                        شروع آزمون
-                                    </Button>
-                                )}
+                                <Button onClick={onAddToCart}>
+                                    <span>شروع آزمون:</span>
+                                    <div className="flex items-center justify-end">
+                                        {(20000).toLocaleString()}
+                                        <TomanIcon />
+                                    </div>
+                                </Button>
                             </div>
                         </div>
                     </article>
@@ -115,8 +77,8 @@ function TestIntro({
                         <div className="relative">
                             <Image
                                 src="/images/quizzes/1.png"
-                                width="400"
-                                height="300"
+                                width={400}
+                                height={300}
                                 alt="تست"
                             />
                         </div>
@@ -124,5 +86,17 @@ function TestIntro({
                 </div>
             </div>
         </div>
+    );
+}
+
+function TestDetail({ icon, label, value }) {
+    return (
+        <li className="flex flex-col gap-4">
+            <span className="font-bold text-lg flex items-center gap-2">
+                {icon}
+                <span>{label}</span>
+            </span>
+            <span>{value}</span>
+        </li>
     );
 }
